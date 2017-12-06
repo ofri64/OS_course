e2e521dc452ebcdf984fbffdb9ae386cd1d503d9
ofri64 <ofri64@gmail.com>
2017-12-06 00:06:21 -0800

    changed user file

--- hw3/rec_char_dev/user_write_and_read.c
+++ hw3/rec_char_dev/user_write_and_read.c
@@ -19,8 +19,18 @@ int main()
         exit(-1);
     }
 
+    printf("file descriptor num is %d\n", file_desc);
+    char buff[256];
     ret_val = write( file_desc, "Hello", 5);
-    ret_val = read(  file_desc, NULL, 100 );
+    ret_val = read(  file_desc, buff, 100 );
+    if (ret_val < 0){
+        printf("Can't read device file: %s\n", "simple_char_dev");
+        fprintf(stderr, "file error is%s\n", strerror(errno));
+    }
+    else{
+        printf("read into buffer the string %s\n", buff);
+    }
+
 
     close(file_desc);
     return 0;
