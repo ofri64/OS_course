6704b86550ebf5943c76e7c70b1ad37ebad9bca3
ofri64 <ofri64@gmail.com>
2017-12-23 05:20:19 -0800

    wrote some code for the multi thread implementation. need to check it

--- hw4/ex4.c
+++ hw4/ex4.c
@@ -9,14 +9,15 @@
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-#define CHUNK_SIZE 1024
+#define CHUNK_SIZE 2
 #define CREATE_PERMISSIONS 0666
 #define NUM_ARGS_ERROR "Error: You must supply at least 2 arguments - one output file and at least 1 input file\n"
 #define MEMORY_ERROR "Error: Memory allocation failed\n"
 #define OPEN_ERROR "Error: Cannot open file from path %s\n"
 #define READ_ERROR "Error: Cannot read from file %s\n"
-#define WRITE_ERROR "Error: Could not write to output file%s\n"
+#define WRITE_ERROR "Error: Could not write to output file\n"
 #define WELCOME_MESSAGE "Hello, creating %s from %d input files\n"
 #define CREATE_THREAD_ERROR "Error: Could not create thread for input file %d with the following error %s\n"
 #define LOCK_ERROR "Error: An error has occurred trying to acquire shared buffer lock: %s\n"
@@ -25,14 +26,17 @@
 
 void* xorChuckInputFile(void*);
 void resetBuffer(char *buffer, int bufferSize);
+void updateSharedInfoOfFiles(bool fileEnded, ssize_t fileBytesRead);
+void writeToOutputBuffer();
+void resetSharedVarsForChunk();
 
 
 int outputFd;
 int numInputFiles;
 char sharedBuffer[CHUNK_SIZE] = {0};
+ssize_t maxBytesReadForCurrentChunk = 0;
 int numFileFinishedChunk = 0;
 int numFilesEnded = 0;
-int readOffset = 0;
 pthread_mutex_t outBufferLock;
 pthread_mutex_t filesProgressLock;
 pthread_cond_t  endChunkCondVar;
@@ -74,7 +78,7 @@ int main (int argc, char *argv[]) {
 
     // Lunch a thread for each input file
 
-    for( int i = 0; i < numInputFiles; ++i) {
+    for (int i = 0; i < numInputFiles; ++i) {
         printf("Main: creating thread for input file %d\n", i);
         errorStatus = pthread_create(&threads[i], NULL, xorChuckInputFile, (void *) argv[i+2]);
         if(errorStatus) {
@@ -83,7 +87,7 @@ int main (int argc, char *argv[]) {
         }
     }
 
-    // Wait for thread to finish
+    // Wait for threads to finish
     for( int i = 0; i < numInputFiles; ++i) {
         pthread_t threadNum = threads[i];
         errorStatus = pthread_join(threadNum, NULL);
@@ -91,60 +95,95 @@ int main (int argc, char *argv[]) {
             printf(JOIN_ERROR, strerror(errorStatus));
         }
     }
+    printf ("Main(): Waited on %d  threads. Done.\n", numInputFiles);
 
-
-
-
+    // clean up and exit
+    close(outputFd);
     free(threads);
     pthread_mutex_destroy(&outBufferLock);
     pthread_mutex_destroy(&filesProgressLock);
     pthread_cond_destroy(&endChunkCondVar);
-
     pthread_exit(NULL);
 }
 
-
 void* xorChuckInputFile(void* inputFilePath){
+    int threadId = (int )pthread_self();
     char* inputFile = (char *) inputFilePath;
     int errorStatus;
-    printf("Thread for input file %s is starting\n", inputFile);
+    bool fileEnded = false;
+    printf("Thread for input file %s with id %d is starting\n",inputFile, threadId);
 
+    // open file only for reading and initialize local input buffer
     int fd = open(inputFile, O_RDONLY);
-    if (fd < 0){
+    if (fd < 0) {
         printf(OPEN_ERROR, inputFile);
         exit(-1);
     }
-
-    // initialize thread local input buffer for one chunk of data
     char inputBuffer[CHUNK_SIZE];
-    ssize_t bytesRead = read(fd, inputBuffer, CHUNK_SIZE);
-    if (bytesRead < 0){
-        printf(READ_ERROR, inputFile);
-        exit(-1);
+
+    while (!fileEnded) {
+
+        // read chunk size (or less) data from the file
+        ssize_t bytesRead = read(fd, inputBuffer, CHUNK_SIZE);
+        if (bytesRead < 0) {
+            printf(READ_ERROR, inputFile);
+            exit(-1);
+        }
+
+        // Acquire lock before updating the shared output buffer data
+        errorStatus = pthread_mutex_lock(&outBufferLock);
+        if (errorStatus) {
+            printf(LOCK_ERROR, strerror(errorStatus));
+            exit(-1);
+        }
+
+        for (int i = 0; i < bytesRead; ++i) {
+            sharedBuffer[i] = sharedBuffer[i] ^ inputBuffer[i];
+        }
+
+        errorStatus = pthread_mutex_unlock(&outBufferLock);
+        if (errorStatus) {
+            printf(UNLOCK_ERROR, strerror(errorStatus));
+            exit(-1);
+        }
+
+        // reset thread input buffer and conclude if thread file has ended
+        resetBuffer(inputBuffer, CHUNK_SIZE);
+        fileEnded = bytesRead < CHUNK_SIZE;
+
+        // Acquire lock before updating shared info about files
+        errorStatus = pthread_mutex_lock(&filesProgressLock);
+        if (errorStatus) {
+            printf(LOCK_ERROR, strerror(errorStatus));
+            exit(-1);
+        }
+
+        updateSharedInfoOfFiles(fileEnded, bytesRead);
+
+        // If you are not the last thread updating the shared output buffer - sleep and wait for the chunk to end
+        while (numFileFinishedChunk < numInputFiles) {
+            printf("Thread for file %d, is now meditating and waiting for other threads\n", threadId);
+            pthread_cond_wait(&endChunkCondVar, &filesProgressLock);
+            printf("Signal condition received in thread %d\n", threadId);
+        }
+
+        // If you are the last thread to finish - write from output buffer to output file,
+        // also reset shared buffer and variables and finally release lock and signal event
+        while (numFileFinishedChunk == numInputFiles) {
+            writeToOutputBuffer();
+            resetSharedVarsForChunk();
+            printf("Sending end of chunk signal from thread %d\n", threadId);
+            pthread_cond_signal(&endChunkCondVar);
+            printf("Releasing files progress lock and starting next chunk phase\n");
+            errorStatus = pthread_mutex_unlock(&filesProgressLock);
+            if (errorStatus) {
+                printf(UNLOCK_ERROR, strerror(errorStatus));
+                exit(-1);
+            }
+        }
     }
 
     close(fd);
-
-    // Acquire lock before updating the shared output buffer data
-    errorStatus = pthread_mutex_lock(&outBufferLock);
-    if(errorStatus) {
-        printf(LOCK_ERROR, strerror(errorStatus));
-        exit(-1);
-    }
-
-    for (int i = 0; i < bytesRead; ++i){
-        sharedBuffer[i] = sharedBuffer[i] ^ inputBuffer[i];
-    }
-
-    errorStatus = pthread_mutex_unlock(&outBufferLock);
-    if (errorStatus){
-        printf(UNLOCK_ERROR, strerror(errorStatus));
-        exit(-1);
-    }
-
-    printf("Finished updating output buffer for thread with input file %s\n", inputFile);
-
-
     pthread_exit(NULL);
 }
 
@@ -153,4 +192,31 @@ void resetBuffer(char *buffer, int bufferSize){
     for (int i = 0; i < bufferSize; i++){
         buffer[i] = 0;
     }
+}
+
+void updateSharedInfoOfFiles(bool fileEnded, ssize_t fileBytesRead){
+    numFileFinishedChunk++;
+    if (fileBytesRead > maxBytesReadForCurrentChunk){
+        maxBytesReadForCurrentChunk = fileBytesRead;
+    }
+    if (fileEnded){
+        numFilesEnded++;
+    }
+}
+
+void writeToOutputBuffer(){
+    for (int i = 0; i < maxBytesReadForCurrentChunk; ++i){
+        ssize_t byteWritten = write(outputFd, sharedBuffer, (size_t ) maxBytesReadForCurrentChunk);
+        if (byteWritten != maxBytesReadForCurrentChunk){
+            printf(WRITE_ERROR);
+            perror(strerror(errno));
+            exit(-1);
+        }
+    }
+}
+
+void resetSharedVarsForChunk(){
+    resetBuffer(sharedBuffer, CHUNK_SIZE);
+    maxBytesReadForCurrentChunk = 0;
+    numFilesEnded = 0;
 }
