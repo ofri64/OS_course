dca46fc2496c405cf1b1674cd36553a48a254abd
ofri64 <ofri64@gmail.com>
2018-01-05 06:38:15 -0800

    saving current architecture which is problematic.

--- hw5/pcc_server.c
+++ hw5/pcc_server.c
@@ -2,25 +2,7 @@
 // Created by okleinfeld on 1/5/18.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-
-#define PROGRAM_ARG_ERROR "Error: You must specify only one value between 0 and 65535, representing the desired port number\n"
-#define SOCKET_CREATE_ERROR "Error: Failed to create a socket due to following error: %s\n"
-
-bool isPrintableCharacter(char);
-int getPortNumber(char* string);
-
-
-pthread_mutex_t pccLock;
+#include "pcc_server.h"
 
 int main(int argc, char *argv[]){
     if (argc != 2){
@@ -43,6 +25,7 @@ int main(int argc, char *argv[]){
         exit(-1);
     }
 
+    // Prepare the server protocol, ip, and port variables before binding it to the socket
     struct sockaddr_in serverAddress;
     socklen_t addressSize = sizeof(struct sockaddr_in);
     memset(&serverAddress, 0, addressSize); // fill the structure with 0 on all bytes
@@ -50,8 +33,69 @@ int main(int argc, char *argv[]){
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddress.sin_port = htons(port);
 
+    // Bind the server values (including the given port number) to the socket
+    int bindStatus = bind(listenFd, (struct sockaddr*) &serverAddress, addressSize);
+    if (bindStatus < 0){
+        printf(BIND_ERROR, strerror(errno));
+        exit(-1);
+    }
+
+    // Mark this socket as a passive socket that will be used to accept connection
+    int listenStatue = listen(listenFd, MAX_LISTEN_QUEUE);
+    if (listenStatue < 0){
+        printf(LISTEN_ERROR, strerror(errno));
+        exit(-1);
+    }
+
+    // Preparation steps before accepting connections
+    pthread_mutex_t pccLock;
+    int initError = pthread_mutex_init(&pccLock, NULL);
+    if (initError != 0){
+        printf(MUTEX_INIT_ERROR, strerror(errno));
+        exit(-1);
+    }
+
+    // Create a connection list - data structure (linked list) for open connections
+    CONNECTIONS_LIST list;
+    list.head = NULL;
+
+    // From now on - accept connections and manage every connection in a different thread
+    while (1){
+        // Create new socket for new connection (identified by four tuple ip-port ip-port)
+        int connFd = accept(listenFd, NULL, NULL);
+        if (connFd < 0){
+            printf(ACCEPT_ERROR, strerror(errno));
+            exit(-1);
+        }
+
+        // Prepare data for a new connection handled in a separate thread
+
+        // Create a new connection object
+        CONNECTION* connection = createConnection(connFd, &pccLock, ppc_total);
+        if (connection == NULL){
+            printf(MEMORY_ALLOC_ERROR);
+            continue;
+        }
+
+        // Initiate thread data structure
+        THREAD_ATTR* connectionThread = assignThreadAttributes(connection, &list);
+        if (connectionThread == NULL){
+            printf(MEMORY_ALLOC_ERROR);
+            continue;
+        }
+        // Create new Thread
+        int threadError = pthread_create(&connection->threadId, NULL, connectionResponse, (void *) connectionThread);
+        if (threadError != 0){
+            printf(THREAD_CREATE_ERROR, strerror(errno));
+            continue;
+        }
+
+        break;
+    }
 
 
+
+    pthread_mutex_destroy(&pccLock);
     close(listenFd);
 }
 
@@ -79,4 +123,74 @@ int getPortNumber(char* string){
     }
 
     return portNum;
+}
+
+CONNECTION* createConnection(int connectionFd, pthread_mutex_t* lock, int* sharedPPC){
+    CONNECTION* connection = (CONNECTION*) malloc(sizeof(CONNECTION));
+    if (connection == NULL){
+        return NULL;
+    }
+
+    connection->connectionFd = connectionFd;
+    connection->sharedLock = lock;
+    connection->sharedPCC = sharedPPC;
+    connection->threadId = 0;
+    connection->next = NULL;
+    return connection;
+}
+
+void destroyConnection(CONNECTION* connection){
+    if (connection != NULL){
+        free(connection);
+    }
+}
+
+void addConnectionToList(CONNECTIONS_LIST* list, CONNECTION* connection){
+    if (list == NULL || connection == NULL){
+        return;
+    }
+    if (list->head == NULL){
+        list->head = connection;
+    } else{
+        connection->next = list->head;
+        list->head = connection;
+    }
+}
+
+void removeConnectionFromList(CONNECTIONS_LIST* list, CONNECTION* connection){
+    if (list == NULL || connection == NULL){
+        return;
+    }
+
+    CONNECTION* current = list->head;
+    if (current == connection) { // special case - remove head of list
+        list->head = connection->next;
+        return;
+    }
+
+    while (current->next != NULL){
+        if (current->next == connection){ // found the connection we want to remove
+            current->next = connection->next;
+            return;
+        }
+        else{
+            current = current->next;
+        }
+    }
+}
+
+THREAD_ATTR* assignThreadAttributes(CONNECTION* connection, CONNECTIONS_LIST* connectionsList){
+    THREAD_ATTR* threadAttribute = (THREAD_ATTR*) malloc(sizeof(THREAD_ATTR));
+    if (threadAttribute == NULL){
+        return NULL;
+    }
+
+    threadAttribute->connection = connection;
+    threadAttribute->connectionsList = connectionsList;
+    return threadAttribute;
+}
+void destroyThreadAttributes(THREAD_ATTR* threadAttributes){
+    if (threadAttributes != NULL){
+        free(threadAttributes);
+    }
 }
---
+++ hw5/pcc_server.h
@@ -0,0 +1,58 @@
+//
+// Created by okleinfeld on 1/5/18.
+//
+
+#ifndef OS_COURSE_PCC_SERVER_H
+#define OS_COURSE_PCC_SERVER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define MAX_LISTEN_QUEUE 100
+#define THREAD_READ_BUFFER_SIZE 1024
+#define PROGRAM_ARG_ERROR "Error: You must specify only one value between 0 and 65535, representing the desired port number\n"
+#define SOCKET_CREATE_ERROR "Error: Failed to create a socket due to the following error: %s\n"
+#define BIND_ERROR "Error: Couldn't bind socket due to the following error: %s\n"
+#define LISTEN_ERROR "Error: Listen command failed for the socket due to the following error: %s\n"
+#define ACCEPT_ERROR "Error: Failed to create a new connection due to the following error: %s\n"
+#define MUTEX_INIT_ERROR "Error: Failed to initiate mutex due to the following error: %s\n"
+#define MEMORY_ALLOC_ERROR "Error: Memory allocation error, connection could not be established\n"
+#define THREAD_CREATE_ERROR "Error: Failed to create a new thread to handle connection due to the following error: %s\n"
+
+typedef struct connection{
+    int connectionFd;
+    pthread_t threadId;
+    pthread_mutex_t* sharedLock;
+    int* sharedPCC;
+    struct connection* next;
+} CONNECTION;
+
+typedef struct connections_list{
+    CONNECTION* head;
+} CONNECTIONS_LIST;
+
+typedef struct thread_attributes{
+    CONNECTION* connection;
+    CONNECTIONS_LIST* connectionsList;
+} THREAD_ATTR;
+
+CONNECTION* createConnection(int connectionFd, pthread_mutex_t* lock, int* sharedPPC);
+void destroyConnection(CONNECTION* connection);
+void addConnectionToList(CONNECTIONS_LIST* list, CONNECTION* connection);
+void removeConnectionFromList(CONNECTIONS_LIST* list, CONNECTION* connection);
+THREAD_ATTR* assignThreadAttributes(CONNECTION* connection, CONNECTIONS_LIST* connectionsList);
+void destroyThreadAttributes(THREAD_ATTR* threadAttributes);
+bool isPrintableCharacter(char c);
+int getPortNumber(char* string);
+void* connectionResponse(void* threadAttributes);
+
+#endif //OS_COURSE_PCC_SERVER_H
